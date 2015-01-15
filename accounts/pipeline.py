75faab71fcb084af4c49e85e37e1294dece7b5be
Seth Hill <seth@pointnineseven.com>
2015-01-15 15:04:41 -0800

    MP-623: Prune unused pipeline functions

--- accounts/pipeline.py
+++ accounts/pipeline.py
@@ -9,73 +9,6 @@ from models import UserData
 import urlparse
 import urllib
 
-@partial
-def require_email(strategy, backend, details, user=None, is_new=False, 
-                  **kwargs):
-    """Make sure that all users come with an email address.
-    """
-    
-    if user and user.email:
-        # Force validation of new email address
-        if backend.name == 'email':
-            return {'is_new': True}
-
-        return
-
-    elif is_new and not details.get('email'):
-        if strategy.session_get('saved_email'):
-            details['email'] = strategy.session_pop('saved_email')
-        else:
-            return redirect('register')
- 
-
-def send_validation_email(strategy, backend, code):
-    print "*** In validate_email"
-    print code.code
-    print code.email
-    print code.verified
-    
-    link = strategy.build_absolute_uri(reverse('social:complete', 
-                                               args=(backend.name,)))
-    link += '?verification_code=%s' % code.code
-
-    print link
-    
-    # TODO: Move this to template
-    message_body = '''
-    Hi, someone gave this email address while signing up for an account on 
-    http://midatlanticocean.org. If it wasn't you, then please discard this email. 
-    
-    Otherwise, click on this link to validate your email address and complete
-    the sign in process:
-    
-        %s
-    
-    Regards, 
-    
-    The MARCO Portal Team
-    ''' % link
-    
-    send_mail('Please validate your email address', message_body, 
-              settings.DEFAULT_FROM_EMAIL, [code.email], fail_silently=False)
-    
-def user_password(strategy, user, backend=None, is_new=False, *args, **kwargs):
-    if backend.name != 'email': 
-        return
-       
-    password = strategy.request_data()['password']
-    if is_new: 
-        user.set_password(password)
-        user.save()
-    
-    elif not user.check_password(password):
-        raise AuthException(strategy.backend)
-
-
-def save_redirect(strategy=None, *args, **kwargs):
-    print "Welcome to save_redirect, kwargs are:", str(kwargs)
-    return {'redirect_to': strategy.request.path}
-
 
 def get_social_details(user, backend, response, *args, **kwargs):
     """Create the UserData table, and gather data from the social provider and
@@ -106,10 +39,7 @@ def get_social_details(user, backend, response, *args, **kwargs):
 
             data.profile_image = url
             data.save()
-        
-    
-    
-    
+
 
 def set_user_permissions(strategy, details, user=None, *args, **kwargs):
     """Configure any initial permissions/groups for the user. 
@@ -131,8 +61,4 @@ def set_user_permissions(strategy, details, user=None, *args, **kwargs):
             user.save()
 
 
-def redirect(redirect_to=None, *args, **kwargs):
-    if redirect_to:
-        print "**** Redirecting to", redirect_to
-#         return HttpResponseRedirect('/account/user-info')
     
