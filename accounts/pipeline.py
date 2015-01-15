dd29b49bdc88aa525048355079779f0de40f6ae2
Seth Hill <seth@pointnineseven.com>
2015-01-15 14:56:40 -0800

    MP-623: #comment Store social user picture URL. The url is refreshed whenever someone signs in.

--- accounts/pipeline.py
+++ accounts/pipeline.py
@@ -5,6 +5,9 @@ from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
 from django.conf import settings
 from social.pipeline.partial import partial
+from models import UserData
+import urlparse
+import urllib
 
 @partial
 def require_email(strategy, backend, details, user=None, is_new=False, 
@@ -73,6 +76,40 @@ def save_redirect(strategy=None, *args, **kwargs):
     print "Welcome to save_redirect, kwargs are:", str(kwargs)
     return {'redirect_to': strategy.request.path}
 
+
+def get_social_details(user, backend, response, *args, **kwargs):
+    """Create the UserData table, and gather data from the social provider and
+    store it in the user data.
+    Right now, we're just extracting the profile picture 
+    """
+    data, _ = UserData.objects.get_or_create(user=user)
+
+    if backend.name == 'facebook':
+        facebook_image_url = 'http://graph.facebook.com/v2.2/{id}/picture'
+        # append ?redirect=false to get a result in JSON
+        id = response.get('id', None)
+        if id:
+            data.profile_image = facebook_image_url.format(id=id)
+            data.save()
+    
+    elif backend.name == 'google-plus':
+        url = response.get('image', {})
+        url = url.get('url')
+        if url:
+            # The default URL provides an image of size 50, we want size 64
+            # so swap out ?sz=50 with ?sz=64 
+            url = urlparse.urlsplit(url)
+            query = urllib.urlencode({'sz': '64'}) 
+            # reassemble
+            url = (url.scheme, url.netloc, url.path, query, url.fragment)
+            url = urlparse.urlunsplit(url)
+
+            data.profile_image = url
+            data.save()
+        
+    
+    
+    
 
 def set_user_permissions(strategy, details, user=None, *args, **kwargs):
     """Configure any initial permissions/groups for the user. 
