f98df59d3c1875c1eb8df649ec011acb98100b98
Seth Hill <seth@pointnineseven.com>
2015-01-16 15:40:43 -0800

    MP-620: Remove the account:login view. Use account:index instead.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     url('^$', 'accounts.views.index', name='index'),
-    url('^login/$', 'accounts.views.login_page', name='login'),
     url('^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, 
         name='logout'), 
     url('^register/$', 'accounts.views.register', name='register'),
