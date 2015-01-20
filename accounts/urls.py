31cf1ec6f1a71ca93bc93c0f32366dee385622c3
Seth Hill <seth@pointnineseven.com>
2015-01-20 11:37:30 -0800

    Mp-620: Get a head start on Django 1.8 urls. I'm very happy to drop the silly patterns object.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.conf import settings
 from django.views.generic import RedirectView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url('^$', 'accounts.views.index', name='index'),
     url('^login/$', RedirectView.as_view(pattern_name='account:index'), 
         name='login'),
@@ -13,10 +13,10 @@ urlpatterns = patterns('',
     url('^verify_new_email$', 'accounts.views.verify_new_email', name='verify_new_email'),
     url('^verify/(?P<code>[a-f0-9]{32})$', 'accounts.views.verify_email', 
         name='verify_email'),
-)
+]
 
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
+    urlpatterns.extend([
         url('^promote-user$', 'accounts.views.promote_user'),
-    )
+    ])
