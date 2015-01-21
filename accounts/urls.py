0e13aa37f2d321a37838985bc136a3d6c88d949a
Seth Hill <seth@pointnineseven.com>
2015-01-20 16:45:25 -0800

    MP-628: Add forgot password url, fix line-length, ditch patterns() in favor of list (for dj1.8)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,22 +1,25 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.conf import settings
 from django.views.generic import RedirectView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url('^$', 'accounts.views.index', name='index'),
     url('^login/$', RedirectView.as_view(pattern_name='account:index'), 
         name='login'),
     url('^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, 
         name='logout'), 
     url('^register/$', 'accounts.views.register', name='register'),
-    url('^confirm-email/$', 'accounts.views.social_confirm_email', name='social_confirm_email'),
-    url('^verify_new_email$', 'accounts.views.verify_new_email', name='verify_new_email'),
+    url('^register/$', 'accounts.views.forgot', name='forgot_password'),
+    url('^confirm-email/$', 'accounts.views.social_confirm_email', 
+        name='social_confirm_email'),
+    url('^verify_new_email$', 'accounts.views.verify_new_email', 
+        name='verify_new_email'),
     url('^verify/(?P<code>[a-f0-9]{32})$', 'accounts.views.verify_email', 
         name='verify_email'),
-)
+]
 
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
+    urlpatterns.extend([
         url('^promote-user$', 'accounts.views.promote_user'),
-    )
+    ])
