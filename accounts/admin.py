511dc9263a3b5ee53cad206c19d5582f636d7edc
Seth Hill <seth@pointnineseven.com>
2015-07-24 14:17:48 -0700

    MP-1012 Add export for User

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from models import PasswordDictionary
 admin.site.register(PasswordDictionary)
@@ -9,3 +11,26 @@ admin.site.register(UserData)
 from models import EmailVerification
 admin.site.register(EmailVerification)
 
+from import_export import fields, resources
+from import_export.admin import ExportMixin
+class UserResource(resources.ModelResource):
+    real_name = fields.Field()
+    preferred_name = fields.Field()
+
+    def dehydrate_real_name(self, user):
+        # "dehydrate_xxx" is a magic method for import export. Evidently,
+        # import-export adds water.
+        return user.userdata.real_name
+
+    def dehydrate_preferred_name(self, user):
+        return user.userdata.preferred_name
+
+    class Meta:
+        model = User
+        fields = ('real_name', 'preferred_name', 'email', 'date_joined', 'last_login', 'is_active',)
+
+class UserAdmin(ExportMixin, UserAdmin):
+    resource_class = UserResource
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
